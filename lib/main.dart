993cb1f2744d8a14c40b04384567baf07bf1efe2
isabinimam <isabinimam@gmail.com>
2023-11-18 01:27:41 +0700

    Change display data to 2x2 Matrix style

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Product Counter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MainMenu(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MainMenu extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Counter'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CounterScreen()),
-                );
-              },
-              child: Text('Start Counter'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Add functionality for other menu options as needed
-              },
-              child: Text('Other Option'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class CounterScreen extends StatefulWidget {
   @override
   _CounterScreenState createState() => _CounterScreenState();
@@ -69,45 +17,72 @@ class _CounterScreenState extends State<CounterScreen> {
         title: Text('Product Counter'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          padding: EdgeInsets.all(10.0),
           children: [
-            Text('Products Passed: $passCount'),
-            Text('Products Reworked: $reworkCount'),
-            Text('Defective Products: $defectiveCount'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Simulate a product passing through
-                setState(() {
-                  passCount++;
-                });
-              },
-              child: Text('Pass Product'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Simulate a product needing rework
-                setState(() {
-                  reworkCount++;
-                });
-              },
-              child: Text('Rework Product'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Simulate a defective product
-                setState(() {
-                  defectiveCount++;
-                });
-              },
-              child: Text('Defective Product'),
-            ),
-            SizedBox(height: 20),
-            Text('Total Products: ${passCount + reworkCount + defectiveCount}'),
+            _buildCategoryCard('Products Passed', passCount, () {
+              setState(() {
+                passCount++;
+              });
+            }),
+            _buildCategoryCard('Products Reworked', reworkCount, () {
+              setState(() {
+                reworkCount++;
+              });
+            }),
+            _buildCategoryCard('Defective Products', defectiveCount, () {
+              setState(() {
+                defectiveCount++;
+              });
+            }),
+            _buildCategoryCard('Total Products', passCount + reworkCount + defectiveCount, null),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildCategoryCard(String category, int count, VoidCallback? onPressed) {
+    return Card(
+      elevation: 5.0,
+      child: InkWell(
+        onTap: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$count',
+                style: TextStyle(fontSize: 54.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10.0),
+              Text(category, textAlign: TextAlign.center),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Product Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CounterScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
